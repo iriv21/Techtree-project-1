7bdcf74709e8e952b83c7d40b1888d3d9d24e707
iriv21 <iri2134286@maricopa.edu>
2021-11-04 09:20:14 -0700

    first commit

---
+++ Guess number.py
@@ -0,0 +1,47 @@
+
+print("Welcome to the GUESSING game!\n")
+
+
+import random
+
+
+def lets_play():
+    from statistics import multimode
+    from statistics import median
+    
+    ready_player = input("Do you want to play the guessing game? Y/N: ")
+    guess_count_list = []
+    while ready_player == "y":
+        random_number = random.randint(1,100)
+        print(random_number)
+        name = input("To begin, enter a name: ")
+        guess_count = 0
+        while lets_play != random_number:
+            try:
+                guess = int(input("Hi {}, guess a number between 1-100: ".format(name)))
+                guess_count +=1
+                pass
+            except ValueError as err:
+                print("Invalid answer. Try again.")
+            if guess < random_number:
+                print("This number is too low, try again.")
+            elif guess > random_number:
+                print("This number is too high, try again.")
+            elif guess == random_number:
+                print("\n{}, you beat the GUESSING game!".format(name))
+                break    
+        print("It took you", guess_count, "tries to beat the game.\n")
+        guess_count_list.append(guess_count)
+        mean = sum(guess_count_list)// len(guess_count_list)
+        print("The mean of your game is ", mean)
+        median = sorted(guess_count_list)
+        print("The median of your game is ", median)
+        mode = multimode(guess_count_list)
+        print("The mode of your game is ", mode)
+        ready_player = input("Do you want to play the guessing game? Y/N: ")
+    else:
+        print("Aww sad to see you go. Goodbye {}".format(name))
+        
+    
+lets_play()
+
